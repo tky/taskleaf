08df10dfadd3dac9004fa662c16dca67b10ee298
t-murata <takayuki.for.public@gmail.com>
2019-04-02 15:50:57 +0900

    Added sepc not to get ohter users tasks.

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,23 +1,34 @@
 require 'rails_helper'
 
-describe "タスク管理機能", type: :system do
+describe 'タスク管理機能', type: :system do
 
-  describe "一覧表示機能" do
+  describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+
     before do
-      user_a = FactoryBot.create(:user, name: "ユーザA", email: "a@example.com")
-      FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
-    context "ユーザAがログインしている" do
-      before do
-        visit login_path
-        fill_in "メールアドレス", with: "a@example.com"
-        fill_in "パスワード", with: "password"
-        click_button 'ログインする'
+    context 'ユーザAがログインしている' do
+      let(:login_user) { user_a }
+
+      it 'ユーザAが表示したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
 
-      it "ユーザAが表示したタスクが表示される" do
-        expect(page).to have_content "最初のタスク"
+    end
+
+    context 'ユーザBがログインしている' do
+      let(:login_user) { user_b }
+
+      it 'ユーザAが作成したタスクが表示されない' do
+        expect(page).not_to have_content '最初のタスク'
       end
     end
   end
