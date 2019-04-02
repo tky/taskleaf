2ee1f36eaa24dd37fa569cdb714f9d893de61526
t-murata <takayuki.for.public@gmail.com>
2019-04-02 15:56:04 +0900

    Added detail page specs.

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
 
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  shared_examples_for 'ユーザAが作成したタスクが評される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
-
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザAがログインしている' do
       let(:login_user) { user_a }
 
-      it 'ユーザAが表示したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
-
+      it_behaves_like 'ユーザAが作成したタスクが評される'
     end
 
     context 'ユーザBがログインしている' do
@@ -31,5 +31,18 @@ describe 'タスク管理機能', type: :system do
         expect(page).not_to have_content '最初のタスク'
       end
     end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザAがログインしている' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like 'ユーザAが作成したタスクが評される'
+    end
+
   end
 end
